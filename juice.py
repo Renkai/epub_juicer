d56d55c7d42a77347a472fb4c8b6bc24dd8eac5a
Renkai Ge <renkaige@tubi.tv>
2022-05-02 16:43:25 +0800

    minor change

--- juice.py
+++ juice.py
@@ -26,6 +26,23 @@ def text_from_item(item):
     return text
 
 
+def get_formations():
+    f = open("reversed-deformations.txt", "r")
+    _origins = set()
+    _formation_map = dict()
+    _dict_words = set()
+    for line in f.readlines():
+        arr = line.split(" ")
+        if len(arr) == 2:
+            (formation, _origin) = arr
+            _origins.add(_origin)
+            _formation_map[formation] = _origin
+            _dict_words.add(formation)
+            _dict_words.add(_origin)
+
+    return _origins, _formation_map, _dict_words
+
+
 if __name__ == '__main__':
     filename = sys.argv[1]
     book = epub.read_epub(filename)
@@ -34,6 +51,11 @@ if __name__ == '__main__':
     # text = text_from_item(example_item)
     # print(text)
     all_words = set()
+    _, formation_map, dict_words = get_formations()
+    print("len ", len(formation_map), len(dict_words))
+    print("contains", 'standoffs' in dict_words)
+    # sys.exit(0)
+
     for item in items:
         text = text_from_item(item)
         # print(text)
@@ -42,20 +64,36 @@ if __name__ == '__main__':
         all_words.update(words)
 
     freqMax = 3.5
-    freqMin = 1.5
+    freqMin = 1
+    not_in_dict = set()
+    origins = set()
+    formations = set()
+
     for word in all_words:
-        if word.endswith('s'):
-            freq = zipf_frequency(word[:-1], 'en')
-            if freqMax > freq > freqMin:
-                # print(word, freq)
-                print(word[:-1])
-        # elif word.endswith('ed'):
-        #     freq = zipf_frequency(word[:-2], 'en')
-        #     if freqMax > freq > freqMin:
-        #         # print(word, freq)
-        #         print(word[:-2])
+        if word not in dict_words:
+            print(word, " not in dict")
+            not_in_dict.add(word)
         else:
-            freq = zipf_frequency(word, 'en')
-            if freqMax > freq > freqMin:
-                # print(word, freq)
-                print(word)
+            if word in formation_map.keys():
+                print("word ", word, " in keys")
+                origin = formation_map[word]
+                freq = zipf_frequency(origin, 'en')
+                if freqMax > freq > freqMin:
+                    origins.add(origin)
+            else:
+                print("else ", word)
+                freq = zipf_frequency(word, 'en')
+                if freqMax > freq > freqMin:
+                    formations.add(word)
+
+    print("not in dict size:", len(not_in_dict))
+    with open('not_in_dict.txt', 'w') as not_in_dict_file:
+        not_in_dict_file.writelines(sorted(not_in_dict))
+
+    print("origins size:", len(origins))
+    with open('origins.txt', 'w') as origins_file:
+        origins_file.writelines(sorted(origins))
+
+    print("formations size:", len(formations))
+    with open('formations.txt', 'w') as formations_file:
+        formations_file.writelines(sorted(formations))
